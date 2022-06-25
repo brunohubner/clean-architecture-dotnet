3b2effc95f3c04d70f21460cfecbb2f9ececa2f8
Bruno Hubner <brunohubnerdev@gmail.com>
2022-06-25 20:50:36 -0300

    feat: create domain layer

---
+++ CleanArchMvc.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+using CleanArchMvc.Domain.Entities;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetById(int? id);
+        Task<Product> Create(Product product);
+        Task<Product> Update(Product product);
+        Task<Product> Remove(Product product);
+    }
+}
