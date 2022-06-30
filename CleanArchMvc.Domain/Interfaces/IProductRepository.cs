bcbf21788c49b52c4cd2d89a200566b1c317bca6
Bruno Hubner <brunohubnerdev@gmail.com>
2022-06-30 18:53:58 -0300

    feat: create database connection

--- CleanArchMvc.Domain/Interfaces/IProductRepository.cs
+++ CleanArchMvc.Domain/Interfaces/IProductRepository.cs
@@ -2,10 +2,10 @@ using CleanArchMvc.Domain.Entities;
 
 namespace CleanArchMvc.Domain.Interfaces
 {
-    interface IProductRepository
+    public interface IProductRepository
     {
         Task<IEnumerable<Product>> GetProducts();
-        Task<Product> GetById(int? id);
+        Task<Product> GetProductCategory(int? id);
         Task<Product> Create(Product product);
         Task<Product> Update(Product product);
         Task<Product> Remove(Product product);
