1f4ebe72e59f587d6f28e1a147435f7022dc09fc
Bruno Hubner <brunohubnerdev@gmail.com>
2022-06-30 19:55:55 -0300

    feat: create migrations

---
+++ CleanArchMvc.Infra.Data/Migrations/populate_database.sql
@@ -0,0 +1,10 @@
+INSERT INTO Categories (Name) VALUES ('Hardware');
+INSERT INTO Categories (Name) VALUES ('Smartphone');
+INSERT INTO Categories (Name) VALUES ('Peripheral');
+ 
+INSERT INTO Products (Name,Description,Price,Stock,Image,CategoryId) VALUES ('AMD Ryzen 5','CPU Processor',2299.99,3,'amd_ryzen.png',1);
+INSERT INTO Products (Name,Description,Price,Stock,Image,CategoryId) VALUES ('GTX 3080','Video Graphics',3599.99,4,'gtx.png',1);
+INSERT INTO Products (Name,Description,Price,Stock,Image,CategoryId) VALUES ('SSD Kingstone','Disk Storage',299.99,12,'ssd.png',1);
+INSERT INTO Products (Name,Description,Price,Stock,Image,CategoryId) VALUES ('IPhone 13 Pro','Apple',8799.99,6,'iphone.jpg',2);
+INSERT INTO Products (Name,Description,Price,Stock,Image,CategoryId) VALUES ('Mouse Razer','Gamer',789.99,16,'mouse.webp',3);
+INSERT INTO Products (Name,Description,Price,Stock,Image,CategoryId) VALUES ('Keyboard Logitech','Mecanic',959.89,8,'keyboard.jpg',3);
