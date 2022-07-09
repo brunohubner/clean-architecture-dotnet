ddb5b8e862dbd68500e3d376f478035b5a5bfc47
Bruno Hubner <brunohubnerdev@gmail.com>
2022-07-09 16:42:54 -0300

    feat: create api models

---
+++ CleanArchMvc.API/Models/LoginModel.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CleanArchMvc.API.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid format email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
